5d08ffce369c33bffffe27f7af8671ac812691f6
Miroslav Bushchuk <21muros2001@gmail.com>
2021-05-01 10:07:13 +0300

    first commit Lab_2, include owl-carousel

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,18 @@
 const testModules = require('./test-module');
 require('../css/app.css');
 
+require('./vendors/jquery.min');
+require('./vendors/owl.carousel.min');
+
+
 /** ******** Your code here! *********** */
 
+// Include my scss styles
+require('../scss/style.scss');
+
 console.log(testModules.hello);
+
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+});
