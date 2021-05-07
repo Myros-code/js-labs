2f3da5ca2c5beab320a3685dcbd610c75a92ccc6
Miroslav Bushchuk <21muros2001@gmail.com>
2021-05-07 19:53:50 +0300

    search ready

--- src/js/lab4/task4/search.js
+++ src/js/lab4/task4/search.js
@@ -24,28 +24,66 @@ module.exports = class Search {
 
   findSimilar(arr) {
     arr.forEach((element) => {
-      if (element["full_name"].search(this.getValue()) == -1 || this.getValue() ==="") {
+      if (this.searchExp(element)) {
       } else {
-        let listItem = `
-        <li class="search-group__item" > 
-            <a href="#!" class="search-group__link" data-card="${element["id"]}">
-                ${element["full_name"]}, ${element["age"]}, ${element["note"]}
-            </a>
-        </li>`;
+        let listItem = this.createItem(element, this.searchFindStr(element));
         this.findList.fillList(listItem);
       }
     });
   }
 
-  listenClick(){
+  createItem(element,str) {
+    return `
+    <li class="search-group__item" > 
+        <a href="#!" class="search-group__link" data-card="${element["id"]}">
+            ${str}
+        </a>
+    </li>`;
+  }
+
+  getItemStr(element) {
+    return `${element["full_name"]}, ${element["age"]}, ${element["note"]}`;
+  }
+
+  searchExp(element) {
+    return (
+      (element["full_name"].search(this.getValue()) == -1 &&
+        element["age"].toString().search(this.getValue()) == -1 &&
+        element["note"].toString().search(this.getValue()) == -1) ||
+      this.getValue() === ""
+    );
+  }
+
+  searchFindStr(element) {
+    if (element["full_name"].search(this.getValue()) !== -1) {
+      let str = element["full_name"];
+      return `${this.insertMark(str, str.search(this.getValue()),this.getValue().length)}, ${element["age"]}, ${element["note"]}`;
+    } else if (element["age"].toString().search(this.getValue()) !== -1) {
+      let str = element["age"].toString();
+      return `${element["full_name"]}, ${this.insertMark(str, str.search(this.getValue()), this.getValue().length)}, ${element["note"]}`;
+    } else {
+      let str = element["note"];
+      return `${element["full_name"]}, ${element["age"]}, ${this.insertMark(str, str.search(this.getValue()), this.getValue().length)}`;
+    }
+  }
+
+  listenClick() {
     items = this.findList.getAllItems();
     items.forEach((element) => {
-        element.addEventListener('click', (event) =>{
-            let cardId = event.target.dataset.card;
-            document.getElementById(cardId).click();
-        });
+      element.addEventListener("click", (event) => {
+        let cardId = event.target.dataset.card;
+        document.getElementById(cardId).click();
+      });
     });
   }
 
-
+  insertMark(str, pos, len) {
+    return (
+      str.slice(0, pos) +
+      "<mark>" +
+      str.slice(pos, pos + len) +
+      "</mark>" +
+      str.slice(pos + len)
+    );
+  }
 };
